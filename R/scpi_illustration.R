e6d24a3bcfb7469b45d819ff90f57ee861881079
Matias D. Cattaneo <cattaneo@princeton.edu>
2022-10-18 19:37:54 -0400

    .

--- R/scpi_illustration.R
+++ R/scpi_illustration.R
@@ -11,6 +11,8 @@ rm(list=ls(all=TRUE))
 
 ### Load SCPI package
 library(scpi)
+library(ggplot2)
+library(latex2exp)
 
 ###############################################################################
 # SINGLE TREATED UNIT
@@ -126,6 +128,59 @@ scplot(result = pi.si, fig.path = ".",
        fig.name = "germany_unc", fig.format = "png", plot.range = (1960:2003),
        label.xy = list(x.lab = "Year", y.lab = "GDP per capita (thousand US dollars)"),
        x.ticks = NULL, e.out = T, event.label = list(lab = "Reunification", height = 10))
+
+
+
+########################################################
+# Sensitivity Analysis for 1997 using subgaussian bounds
+########################################################
+set.seed(8894)
+res.si  <- scpi(data = df, sims = sims, e.method = "gaussian", e.order = e.order, e.lags = e.lags,
+                u.order = u.order, u.lags = u.lags, u.sigma = u.sigma, u.missp = u.missp,
+                cores = cores, w.constr = list(name = "simplex"), lgapp = "linear") 
+
+e.alpha <- 0.05  # default level in scpi
+sens <- c(0.25, 0.5, 1, 1.5, 2)
+time <- c(1997)
+emean <- res.si$inference.results$e.mean
+esig <- sqrt(res.si$inference.results$e.var)
+sc.l.0 <- res.si$inference.results$CI.in.sample[,1,drop = F]
+sc.r.0 <- res.si$inference.results$CI.in.sample[,2,drop = F]
+y <- res.si$data$Y.post
+
+for (l in 1:length(time)) {
+  ssc.l.1 <- ssc.r.1 <- c()
+  e.mean <- emean[time[l]-1990]
+  sig <- esig[time[l]-1990]
+  sig.seq <- sens*sig
+  
+  for (s in 1:length(sig.seq)) {
+    eps  <- sqrt(-log(e.alpha/2)*2*(sig.seq[s]^2))
+    ssc.l.1[s] <- sc.l.0[time[l]-1990] + e.mean - eps
+    ssc.r.1[s] <- sc.r.0[time[l]-1990] + e.mean + eps
+  }
+  
+  sen.dat <- data.frame(t=c(1:5), lb1=ssc.l.1, ub1=ssc.r.1,
+                        lb=rep(sc.l.0[time[l]-1990], 5),
+                        ub=rep(sc.r.0[time[l]-1990], 5),
+                        lab=as.factor(sens))
+  plot <- ggplot() + theme_bw() +
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank()) +
+    labs(x="sd. of e", y="GDP per capita (thousand US dollars)")
+  plot <- plot + geom_errorbar(data=sen.dat, aes(x=lab, ymin=lb1, ymax=ub1),
+                               col="maroon", width=0.2, linetype=5) +
+    geom_errorbar(data=sen.dat, aes(x=lab, ymin=lb, ymax=ub),
+                  col="blue", width=0.2, linetype=1) +
+    geom_hline(yintercept = y[time[l]-1990], linetype=1, size=0.3, alpha=0.8) +
+    annotate("text", x=5.4, y=y[time[l]-1990]-.1,label="Y(1)", size=3.5) +
+    scale_x_discrete(labels=c(parse(text=TeX("$0.25\\hat{\\sigma}$")),
+                              parse(text=TeX("$0.5\\hat{\\sigma}$")),
+                              parse(text=TeX("$\\hat{\\sigma}$")),
+                              parse(text=TeX("$1.5\\hat{\\sigma}$")),
+                              parse(text=TeX("$2\\hat{\\sigma}$"))))
+  ggsave(paste("sensitivity", l, ".png", sep=""), width=6, height=4.5, dpi=1000)
+}
 
 
 
